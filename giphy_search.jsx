78f0d528fcaa9c3689bb0d3afd465def54117080
Elly <ellychau10@yahoo.com>
2021-12-10 12:14:02 -0800

    Add fetchGiphysSearch on util/api_util.js

--- giphy_search.jsx
+++ giphy_search.jsx
@@ -2,3 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import configureStore from './store/store';
 import Root from './components/root';
+
+// testing only
+import fetchGiphysSearch from './util/api_util';
---
+++ util/api_util.js
@@ -0,0 +1,9 @@
+
+export const fetchGiphysSearch = searchTerm => (
+  $.ajax({
+    method: "GET",
+    url: `http://api.giphy.com/v1/gifs/search?q=${searchTerm}&api_key=h0TXriBXWwTDGKcgVWIDQhbY6xr6FSxs&limit=2`
+  })
+)
+
+window.fetchGiphysSearch = fetchGiphysSearch;
