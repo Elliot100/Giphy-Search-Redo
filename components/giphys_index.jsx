9673e48ca0c75d2b71d25b3b4125c8bfc3aee03b
Elly <ellychau10@yahoo.com>
2021-12-11 15:34:45 -0800

    Update GiphysSearch component and GiphyIndex component

--- components/giphys_index.jsx
+++ components/giphys_index.jsx
@@ -1,3 +1,13 @@
 import React from 'react';
 
 import GiphysIndexItem from './giphys_index_item';
+
+function GiphyIndex (giphys) {
+  return (
+    <ul>
+      { giphys.map(giphy => <GiphysIndexItem key={giphy.id} giphy={giphy} />) }
+    </ul>
+  );
+}
+
+export default GiphyIndex;
